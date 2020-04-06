d58cabf2ae0a3b908e8aa164c52017a946aa374c
dschoon <dschoon@gmail.com>
2020-04-06 10:47:50 -0500

    Trying to fix iframe

--- src/templates/payment-page.js
+++ src/templates/payment-page.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
 
 import Layout from '../components/Layout';
-import { InjectedCheckoutForm } from '../components/StripePayment';
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_zGuxi8G0lnNaj6b4Idx23FVB009bvpYm6o');
 
-const ELEMENTS_OPTIONS = {
-  fonts: [
-    {
-      cssSrc: 'https://fonts.googleapis.com/css?family=Roboto',
-    },
-  ],
-};
-
-export const PaymentPageTemplate = ({
-  heading,
-  mainpitch,
-}) => (
-  <div className="home-page">
+export const PaymentPageTemplate = () => (
+  <div className="payment-page">
     <section className="section section--gradient">
       <div className="container">
         <div className="section">
           <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="payment-content">
-                <iframe width="480" height="480" src="https://api.pico.tools/pn/castercommunity/7ya952k4" frameBorder="0"/>
-              </div>
+            <div className="payment-content">
+              <iframe width="480" height="480" src="https://api.pico.tools/pn/castercommunity/7ya952k4" frameBorder="0"/>
             </div>
           </div>
         </div>
@@ -50,10 +31,7 @@ const PaymentPage = ({ data }) => {
 
   return (
     <Layout>
-      <PaymentPageTemplate
-        heading={frontmatter.heading}
-        mainpitch={frontmatter.mainpitch}
-      />
+      <PaymentPageTemplate />
     </Layout>
   )
 };
